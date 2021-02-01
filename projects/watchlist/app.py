b167063061c9d26571906eedd59d00bb0ba55c3c
MarkJiYuan <zhengjiy16@163.com>
2021-02-01 20:00:21 +0800

    使用模版写了主页

--- projects/watchlist/app.py
+++ projects/watchlist/app.py
@@ -1,5 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 # 有不懂的别着急，继续敲就好了
 @app.route('/1')
@@ -9,4 +23,10 @@ def hello():
 @app.route('/2')
 def byebye():
     return '再见！李姝瑶！'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
 
